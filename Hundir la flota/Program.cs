0552ddd5cbdd9ec051b3e4f6bf50276011ff3ed3
Mario Martínez Vitutia <9205625@alumnos.ufv.es>
2023-03-18 11:13:42 +0100

    Añadido Menú principal

--- Hundir la flota/Program.cs
+++ Hundir la flota/Program.cs
@@ -10,6 +10,22 @@ namespace Hundir_la_flota
     {
         static void Main(string[] args)
         {
+            //Declaro al variable de opción controlando que sea un entero y llamo al menú
+            if (int.TryParse(Console.ReadLine(), out int opcion)) { }
+            MenuPrincipal();
+
+        }
+        static int MenuPrincipal(){
+            //Declaro una variable de opción
+            if (int.TryParse(Console.ReadLine(), out int opcion)) { }
+            //Imprimo por pantalla las diferentes opciones
+            Console.WriteLine("MENÚ PRINCIPAL");
+            Console.WriteLine("1. Un Jugador");
+            Console.WriteLine("2. Dos Jugadores");
+            Console.WriteLine("3. Cargar Partida");
+            Console.WriteLine("4. Ranking");
+            Console.WriteLine("5. Salir");
+            return opcion;
         }
     }
 }
